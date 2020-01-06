a379d4eee09cbbdd1cbc2c3082dc9542e77b9e82
Mahendra Choudhary <jakepintu@gmail.com>
2020-01-06 11:14:52 +0530

    Fetching data

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,41 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Posts from './Posts'
 
+import { firestore } from '../firebase'
+
 const Application = () => {
-  const [posts, setPosts] = useState([
-    {
-      id: '1',
-      title: 'A Very Hot Take',
-      content:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis suscipit repellendus modi unde cumque, fugit in ad necessitatibus eos sed quasi et! Commodi repudiandae tempora ipsum fugiat. Quam, officia excepturi!',
-      user: {
-        uid: '123',
-        displayName: 'Bill Murray',
-        email: 'billmurray@mailinator.com',
-        photoURL: 'https://www.fillmurray.com/300/300'
-      },
-      stars: 1,
-      comments: 47
-    },
-    {
-      id: '2',
-      title: 'The Sauciest of Opinions',
-      content:
-        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis suscipit repellendus modi unde cumque, fugit in ad necessitatibus eos sed quasi et! Commodi repudiandae tempora ipsum fugiat. Quam, officia excepturi!',
-      user: {
-        uid: '456',
-        displayName: 'Mill Burray',
-        email: 'notbillmurray@mailinator.com',
-        photoURL: 'https://www.fillmurray.com/400/400'
-      },
-      stars: 3,
-      comments: 0
-    }
-  ])
+  const [posts, setPosts] = useState([])
 
   const handleCreate = post => {
     setPosts([...posts, post])
   }
+
+  useEffect(() => {
+    ;(async () => {
+      const snapshot = await firestore.collection('posts').get()
+
+      const snapshotData = snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }))
+
+      setPosts(snapshotData)
+    })()
+  }, [])
 
   return (
     <main className="Application">
